4f27a592c424b8ca2064b9c8ae9d001659c03467
hihi-git <20201111@duksung.ac.kr>
2022-03-23 22:00:00 +0000

    뒤집은 소수

---
+++ inflearn/뒤집은 소수.py
@@ -0,0 +1,28 @@
+n = int(input())
+# nums = list(map(int, input().split()))
+nums = [32, 55, 62, 3700, 250]
+
+
+# 숫자 뒤집음
+# 끝자리 0 이면 첫 자리부터 연속된 0 무시
+def reverse(x):
+    r = reversed(str(x))
+    r = ''.join(r)
+    r = r.strip('0')
+    return int(r)
+
+# 소수인지 확인
+def isPrime(x):
+    if x == 1:
+        return False
+    for i in range(2, x//2+1):
+        if x%i == 0:
+            return False    # 소수가 아님
+    else:
+        return True
+
+
+for num in nums:
+    x = reverse(num)
+    if isPrime(x):
+        print(x, end=' ')
