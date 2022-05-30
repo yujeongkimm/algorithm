eb214ec1c09ce4c73065b6a1d30346a42a53e5c5
yujeongkimm <20201111@duksung.ac.kr>
2022-05-30 22:23:14 +0900

    카드 역배치

--- inflearn/탐색&시뮬레이션/카드 역배치.py
+++ inflearn/탐색&시뮬레이션/카드 역배치.py
@@ -1,11 +1,9 @@
 # 리스트 생성
 list = list(range(1, 21))
 # 입력 10번 반복
-for i in range(0, 10):
+for _ in range(0, 10):
     a, b = map(int, input().split())
     # 카드 역배치
     for n in range((b-a+1)//2):
-        num = list[a-1+n]
-        list[a-1+n] = list[b-1-n]
-        list[b-1-n] = num
+        list[a-1+n] , list[b-1-n] = list[b-1-n], list[a-1+n]
 print(list)
