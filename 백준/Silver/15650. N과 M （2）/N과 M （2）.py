a778c13597e94f6916983d9c546c82bed13c203f
u__j <84445210+yujeongkimm@users.noreply.github.com>
2023-03-14 11:22:37 +0900

    [Silver III] Title: N과 M (2), Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -0,0 +1,15 @@
+import sys
+n,m = map(int, sys.stdin.readline().split(' '))
+arr = [0 for _ in range(m)]
+def dfs(cnt):
+    if cnt==m:
+        for i in arr:
+            sys.stdout.write(str(i) + ' ')
+        sys.stdout.write('\n')
+    else:
+        start=1 if cnt==0 else arr[cnt-1]+1
+        for i in range(start, n+1):
+            arr[cnt]=i
+            dfs(cnt+1)
+            arr[cnt]=0
+dfs(0)
