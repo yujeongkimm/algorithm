bc8d791963debf6b4e2155e8b7856692ef2f1a43
u__j <84445210+yujeongkimm@users.noreply.github.com>
2023-06-22 17:38:50 +0900

    [Silver II] Title: 나무 자르기, Time: 520 ms, Memory: 261196 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -4,10 +4,11 @@ arr.sort()
 # 절단기 높이 = mid
 # 자른 나무 길이 < m => 가져가는 나무 길이 늘림 => 절단기 높이 줄어야함 => 범위 less
 # 자른 나무 길이 > m => 성공 => 최소값을 만족하는 절단기 높이 구해야함 => 가져가는 나무 길이 줄임 => 절단기 높이 늘림 => 범위 more
-# 자른 나무 길이 = m => 성공 => 루프문 빠져나가야 함 => start값 조절
+# ❔ 자른 나무 길이 = m => 성공 => 더 높이 잘라도 됨?? =>
 # ❔start가 end보다 커지는 순간 => end가 절단기 높이 최대값
 # 10 15 17 20
 start,end=0,max(arr)
+result=0
 while start<=end:
     mid=(start+end)//2
     res=0
@@ -17,5 +18,6 @@ while start<=end:
     if res<m:
         end=mid-1
     else:
+        result = mid
         start=mid+1
-print(end)
+print(result)
