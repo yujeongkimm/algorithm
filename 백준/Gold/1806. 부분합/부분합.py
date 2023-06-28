dcbd6b5e16fe8116662dd4a6e843d52f80fe34be
u__j <84445210+yujeongkimm@users.noreply.github.com>
2023-06-28 16:27:46 +0900

    [Gold IV] Title: 부분합, Time: 116 ms, Memory: 42340 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.py
+++ 백준/Gold/1806. 부분합/부분합.py
@@ -1,16 +1,16 @@
 n,s=map(int,input().split())
 arr=list(map(int,input().split()))
 l,r=0,0
-sum=arr[0]
 length=100000
+S=arr[0]
 while True:
-    if sum>=s:
-        length=min(length,r-l+1)    # 최소길이 갱신
-        sum-=arr[l]
+    if S >= s:    # 최소 길이 비교, sum, l 갱신
+        length=min(length,r-l+1)
+        S-=arr[l]
         l+=1
-    else: # 누적합이 작으면 end 늘림
-        r += 1
+    else:
+        r+=1
         if r==n:
             break
-        sum+=arr[r]
+        S+=arr[r]
 print(0) if length==100000 else print(length)
