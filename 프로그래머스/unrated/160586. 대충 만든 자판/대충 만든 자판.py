3989574f4afd0225168647313c406cc757be4c6b
may__yy <84445210+yujeongkimm@users.noreply.github.com>
2023-09-06 16:00:59 +0900

    [unrated] Title: 대충 만든 자판, Time: 27.00 ms, Memory: 9.98 MB -BaekjoonHub

--- 프로그래머스/unrated/160586. 대충 만든 자판/대충 만든 자판.py
+++ 프로그래머스/unrated/160586. 대충 만든 자판/대충 만든 자판.py
@@ -1,24 +1,17 @@
-def getPoints(keymap, ch):
-    flag = False
-    time = 101
-    for key in keymap:
-        if ch in key:
-            time = min(key.index(ch)+1, time)
-            flag = True
-    if not flag:
-        time=-1
-    return time
-
-
 def solution(keymap, targets):
     answer = []
     for target in targets:
-        times = 0
-        for ch in target: 
-            time = getPoints(keymap, ch)
-            if time == -1:
+        times=0
+        for ch in target:
+            time=101
+            flag=False
+            for key in keymap:
+                if ch in key:
+                    time=min(key.index(ch)+1, time)
+                    flag=True
+            if not flag:
                 times=-1
                 break
             times+=time
-        answer.append(times)
+        answer.append(times)    
     return answer
