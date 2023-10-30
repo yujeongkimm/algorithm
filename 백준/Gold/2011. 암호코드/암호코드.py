e43fab4603a7522835468657c729e17afdd51e3d
may__yy <84445210+yujeongkimm@users.noreply.github.com>
2023-10-30 16:14:49 +0900

    [Gold V] Title: 암호코드, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/2011. 암호코드/암호코드.py
@@ -0,0 +1,14 @@
+n=input()
+dp=[0] * (len(n)+1) 
+dp[0]=1
+dp[1]=1
+
+if n[0]=='0':
+    print(0)
+else:
+    for i in range(2, len(n)+1):
+        if int(n[i-1])>0:
+            dp[i]=dp[i-1]
+        if 10<= int(n[i-1])+10*int(n[i-2]) <=26:
+            dp[i]+=dp[i-2]
+    print(dp[-1]%1000000)
