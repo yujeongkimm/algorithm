ddc10175f13c12225230214b5b149b354f6c7a90
may__yy <84445210+yujeongkimm@users.noreply.github.com>
2024-01-05 15:01:34 +0900

    [Gold V] Title: 두 용액, Time: 124 ms, Memory: 42036 KB -BaekjoonHub

--- 백준/Gold/2470. 두 용액/두 용액.py
+++ 백준/Gold/2470. 두 용액/두 용액.py
@@ -1,27 +1,19 @@
 n=int(input())
-a=list(map(int,input().split()))
+liquid=list(map(int,input().split()))
+liquid.sort()
 
-# -99 -2 -1 4 98
-# 1. 정렬
-# 2. 가장 왼쪽, 오른쪽 합해서 0이랑 가까운지 확인
-# 2-1. 0보다 크면 오른쪽을 줄이기
-# 2-2. 0보다 작으면 왼쪽 줄이기
-# 2-3. 같으면 리턴
-
-a.sort()
-
-l,r=0,n-1
-final=[a[l],a[r]]
-ans=abs(a[l]+a[r])
-while l<r:
-    total=a[l]+a[r]
-    if abs(total)<ans:
-        ans=abs(total)
-        final=[a[l], a[r]]
-        if ans==0:
-            break
-    if total<0:
-        l+=1
+left,right=0,n-1
+ans_total=abs(liquid[left]+liquid[right])
+ans_left=left
+ans_right=right
+while left<right:
+    total=liquid[left]+liquid[right]
+    if abs(total)<ans_total:
+        ans_total=abs(total)
+        ans_left=left
+        ans_right=right
+    if total>0:
+        right-=1
     else:
-        r-=1
-print(final[0],final[1])
+        left+=1
+print(liquid[ans_left], liquid[ans_right])
