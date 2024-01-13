8ffc77db5f491dd007873a8da739981075dbaf56
may__yy <84445210+yujeongkimm@users.noreply.github.com>
2024-01-13 11:28:16 +0900

    [Silver IV] Title: Router, Time: 3108 ms, Memory: 34000 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Silver/15828. Router/Router.py
@@ -0,0 +1,21 @@
+# FIFO queue
+# 양의 정수는 패킷 들어옴
+# 0 은 라우터가 패킷 하나 처리함
+# -1은 입력의 끝
+from collections import deque
+n=int(input())
+queue=deque()
+
+while True:
+    p=int(input())
+    if p==0:
+        queue.popleft()
+        continue
+    if p==-1:
+        break
+    if len(queue)<n:
+        queue.append(p)
+
+if queue:
+    print(*queue)
+else: print("empty")
